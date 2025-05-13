fcb2a2abd5328a006fcc53ba814398c6f6cfcb5a
Angi-Adema <dr.adema@gmail.com>
2025-05-12 20:16:35 -0600

    created AccountDAOInterface.java and included the logic to createAccount, getAccountByUsername and getAccountByUsernameAndPassword

--- src/main/java/Controller/SocialMediaController.java
+++ src/main/java/Controller/SocialMediaController.java
@@ -38,21 +38,52 @@ public class SocialMediaController {
      * This is an example handler for an example endpoint.
      * @param context The Javalin Context object manages information about both the HTTP request and response.
      */
-    private void registerHandler(Context ctx) throws JsonProcessingException {
+    private void registerHandler(Context ctx) {
+        // Javalin internally uses Jackson's ObjectMapper and abstracts away the boilerplate so we do not have to write or confiture this.
+        // We also do not need to specify the throws JsonProcessingException as Javalin will handle this for us.
         Account acct = ctx.bodyAsClass(Account.class);
 
         if(acct.getUsername() == null || acct.getUsername().isBlank() || acct.getPassword() == null || acct.getPassword().length() < 4) {
             ctx.status(400);
-            ctx.json("Invalid username or password");
             return;
-        } else {
-            
         }
+
+        try {
+            Account newAcct = accountService.register(acct);
+            if (newAcct != null) {
+                // Javalin will automatically return the response as 200 OK as the default along with the JSON object.
+                ctx.status(200).json(newAcct);
+            } else {
+                ctx.status(400).result("Account already exists");
+            }
+        } catch (Exception e) {
+            ctx.status(500).result("Server error: " + e.getMessage());
+            e.printStackTrace();
+        }
+
     }
 
-    private void loginHandler(Contect ctx) throws JsonProcessingException {
-        ObjectMapper objectMapper = new ObjectMapper();
-        context.json("sample text");
+    private void loginHandler(Context ctx) {
+
+        Account loginAttempt = ctx.bodyAsClass(Account.class);
+
+        if(loginAttempt.getUsername() == null || loginAttempt.getUsername().isBlank() || loginAttempt.getPassword() == null || loginAttempt.getPassword().isBlank()) {
+            ctx.status(400);
+            return;
+        }
+
+        try {
+            Account existingAccount = accountService.login(loginAttempt.getUsername(), loginAttempt.getPassword());
+
+            if(existingAccount != null) {
+                ctx.status(200).json(existingAccount);
+            } else {
+                ctx.status(401).result("Invalid username or password");
+            }
+        } catch (Exception e) {
+            ctx.status(500).result("Server error: " + e.getMessage());
+            e.printStackTrace();
+        }
     }
 
 
