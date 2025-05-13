db2c644f338f075a256ed12e0d5af2aa0f266ff6
Angi-Adema <dr.adema@gmail.com>
2025-05-13 10:50:06 -0600

    updated the comments in the AccountDAOInterface.java file

--- src/main/java/DAO/AccountDAOInterface.java
+++ src/main/java/DAO/AccountDAOInterface.java
@@ -2,9 +2,16 @@ package DAO;
 
 import Model.Account;
 
+// Provide for decoupling, scalability and readability by using an interface.
 public interface AccountDAOInterface {
+
+    // Create and insert a new account through registration.
     Account createAccount(Account account);
+
+    // Lookup usernames to be sure that username is not already in use.
     Account getAccountByUsername(String username);
+
+    // Validate login credentials.
     Account getAccountByUsernameAndPassword(String username, String password);
     
 }
